1217eae838d00426440940f571ffaf7eccc58395
Luan Mai <mai.luan@gmail.com>
2015-08-06 02:08:32 +1000

    mssqlwrapper.py: updated

--- mssqlwrapper.py
+++ mssqlwrapper.py
@@ -2,8 +2,8 @@ __author__ = 'Lmai'
 
 import pyodbc
 import logging
-import os
-import binascii
+import uuid
+import itertools
 '''
 Would like to log the query like psycopg2 modgrify
 http://stackoverflow.com/questions/5266430/how-to-see-the-real-sql-query-in-python-cursor-execute
@@ -26,33 +26,61 @@ class DB:
     def check_sql_string(sql, values):
         unique = "%PARAMETER%"
         sql = sql.replace("?", unique)
-        for v in values:
-            sql = sql.replace(unique, repr(v), 1)
-        return '\n'+sql
+        rows = values
+        if rows:
+            if not all(isinstance(e, (list, tuple)) for e in values):   # debug executemany
+                rows = [values]
+            a = [[sql.replace(unique, repr(v), 1) for v in row] for row in rows]
+            a = itertools.chain(*a)
+            return '\n'+'\n'.join(a)
+        return None
+
+    def get_one_value(self, query, *argv):
+        if self.debug:
+            logging.debug(self.check_sql_string(query, argv))
+        row = self._cursor.execute(query, *argv).fetchone()
+        return row[0]
 
     def get_data(self, query, *argv):
         if self.debug:
             logging.debug(self.check_sql_string(query, argv))
         rows = self._cursor.execute(query, *argv).fetchall()
-
         return rows
 
     def execute(self, query, *argv):
         if self.debug:
             logging.debug(self.check_sql_string(query, argv))
 
-        self._cursor.execute(query, *argv)
-
+        self._cursor.execute(query)
         return self._cursor.rowcount
+
+    def executemany(self, query, list_of_tuple):
+        if self.debug:
+            logging.debug(self.check_sql_string(query, list_of_tuple))
+        self._cursor.executemany(query, list_of_tuple)
+        return self._cursor.rowcount
+
+    def sp_columns(self, table_name, catalog=None, schema=None, column=None):
+        logging.debug('lan=%s' % table_name)
+        return [row.column_name for row in self._cursor.columns(table_name)]
 
 
 class TempTable:
 
     @staticmethod
     def create_from_data(db_instance, data, create_qry):
-        table_name = 'tmp_luan' #+ binascii.hexlify(os.urandom(32)).decode('utf-8')
-        print(table_name)
+        table_name = '#tmp_' + uuid.uuid4().get_hex().upper()[:16]
         db_instance.execute(create_qry.format(table_name=table_name))
+        rows = db_instance.sp_columns(table_name+'%')
+        print(rows)
+        no_of_fields = db_instance.get_one_value('''\
+        SELECT count(*)
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME like ?
+        ''', table_name+'%')
 
+        db_instance.executemany('insert into {table_name} values({fields})' \
+                                .format(table_name=table_name,
+                                        fields=','.join(['?']*no_of_fields)), data)
         return table_name
 
