6b373877c608d84ac871d7ab76d1bb94cd5c7e30
Luan Mai <luan.mai@lonsec.com.au>
2015-08-05 17:35:34 +1000

    mssqlwrapper.py: updated: add TempTable class

--- mssqlwrapper.py
+++ mssqlwrapper.py
@@ -2,7 +2,8 @@ __author__ = 'Lmai'
 
 import pyodbc
 import logging
-
+import os
+import binascii
 '''
 Would like to log the query like psycopg2 modgrify
 http://stackoverflow.com/questions/5266430/how-to-see-the-real-sql-query-in-python-cursor-execute
@@ -36,9 +37,22 @@ class DB:
 
         return rows
 
+    def execute(self, query, *argv):
+        if self.debug:
+            logging.debug(self.check_sql_string(query, argv))
+
+        self._cursor.execute(query, *argv)
+
+        return self._cursor.rowcount
+
 
 class TempTable:
 
     @staticmethod
-    def from_data(db_instance, data, field_names):
-        pass
+    def create_from_data(db_instance, data, create_qry):
+        table_name = 'tmp_luan' #+ binascii.hexlify(os.urandom(32)).decode('utf-8')
+        print(table_name)
+        db_instance.execute(create_qry.format(table_name=table_name))
+
+        return table_name
+
